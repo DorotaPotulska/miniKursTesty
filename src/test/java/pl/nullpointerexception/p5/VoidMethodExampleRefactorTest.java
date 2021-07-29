8b8f52be53153e203cf52f1080e987077759afeb
Dorota Potulska <cdc4@wp.pl>
2021-07-29 23:24:11 +0200

    P5-void method

--- src/test/java/pl/nullpointerexception/p5/VoidMethodExampleRefactorTest.java
+++ src/test/java/pl/nullpointerexception/p5/VoidMethodExampleRefactorTest.java
@@ -7,23 +7,37 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class VoidMethodExampleRefactorTest {
 
     private VoidMethodExampleRefactor testee = new VoidMethodExampleRefactor();
 
     @Test
-    public void delete(@TempDir Path tempDir) throws IOException {
+    public void shouldDeleteExistingFile(@TempDir Path tempDir) throws IOException {
         // given
         Files.createFile(tempDir.resolve("testFile.txt"));
         // when
-        testee.delete(tempDir,"testFile.txt");
-
+        boolean result = testee.delete(tempDir, "testFile.txt");
         // then
-
+        assertThat(result).isTrue();
     }
 
+    @Test
+    public void shouldNotDeleteNoneExistingFile(@TempDir Path tempDir) throws IOException {
+        // when
+        boolean result = testee.delete(tempDir, "testFile.txt");
+        // then
+        assertThat(result).isFalse();
+    }
 
-
+    @Test
+    public void shouldNotDeleteNoneEmptyDir(@TempDir Path tempDir) throws IOException {
+        // given
+        Files.createFile(tempDir.resolve("testFile.txt"));
+        // when
+        boolean result = testee.delete(tempDir, "");
+        // then
+        assertThat(result).isFalse();
+    }
 }
